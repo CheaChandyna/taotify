a3236ff875080e7924c2405e3fbd6816b91b2f2c
Kanchnak Khorn <kanchnakkhorn@gmail.com>
2026-03-28 09:55:42 +0700

    added navbar

--- app/src/main/java/com/example/taotify/nagivation/Screen.kt
+++ app/src/main/java/com/example/taotify/nagivation/Screen.kt
@@ -1,6 +1,10 @@
 package com.example.taotify.nagivation
 
+import androidx.compose.ui.graphics.vector.ImageVector
+
 sealed class Screen(val route: String) {
   object Login : Screen("login")
   object Home : Screen("home")
+  object Search : Screen("search")
+  object Library : Screen("library")
 }
